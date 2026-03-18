ad8bd3ce9198ee72ba15f000a611f31fa1d422e5
nakokomochi <nako66namm2@gmail.com>
2026-03-18 22:30:06 +0900

    Update spot.js

--- JS/spot.js
+++ JS/spot.js
@@ -592,3 +592,9 @@ window.addEventListener("DOMContentLoaded", function () {
 window.initMap = function () {
   console.log("initMap動いた！");
 }
+
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 35.681236, lng: 139.767125 },
+    zoom: 14,
+  });
+}
